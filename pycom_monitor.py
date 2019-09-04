13d7cb685ccba082c099d23a029fe061a6ce9852
Stefano Pio Zingaro <stefanopio.zingaro@unibo.it>
2019-09-04 12:51:45 +0200

    license added to the monitor header

--- pycom_monitor.py
+++ pycom_monitor.py
@@ -1,3 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2019 IoT Meets AI Team Challenge 4
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 from machine import I2C, Pin, UART
 
 from lib import adafruit_am2320, adafruit_gps, sds011, adafruit_sgp30, ssd1306
